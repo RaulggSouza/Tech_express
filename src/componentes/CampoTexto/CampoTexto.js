2baea5c8c61768db4e861ce49758b40e7855df23
RaulggSouza <raulggsouza@hotmail.com>
2025-03-12 19:09:47 -0300

    Input number now accepts decimal numbers

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -3,12 +3,14 @@ import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
     const aoDigitado = (evento) => {
-        props.aoAlterado(evento.target.value)
+        let valor = evento.target.value;
+        
+        props.aoAlterado(valor)
     }
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input type={props.type} onChange={aoDigitado} placeholder={props.placeholder} required/>
+            <input type={props.type || "text"} onChange={aoDigitado} placeholder={props.placeholder} step="any" required/>
         </div>
     )
 }
--- src/componentes/Produto/Produto.js
+++ src/componentes/Produto/Produto.js
@@ -9,7 +9,7 @@ const Produto = (props) => {
             </div>
             <div className="rodape">
                 <h4>{props.nome}</h4>
-                <h5>{props.preco}</h5>
+                <h5>R$ {Intl.NumberFormat("pt-br", {style: "currency", currency: "BRL"}).format(props.preco)}</h5>
                 <h6>{props.estado}</h6>
             </div>
         </div>
